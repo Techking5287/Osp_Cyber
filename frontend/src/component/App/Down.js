9ec691eb6af2f5e216de9bd0049f33453750b09e
Full Stack || Blockchain Eng (Techking5287) <techking5287@gmail.com>
2023-10-27 06:31:54 +0000

    email fixed

--- frontend/src/component/App/Down.js
+++ frontend/src/component/App/Down.js
@@ -72,13 +72,22 @@ const Down = () => {
                     </Space>}
                     <Button id="down" hidden {...linkProps} onClick={onDownload}>down</Button>
                 </Modal >
-                <Button type="primary" onClick={onDownload} className="flex">
-                    Click Here to Download the PDF &nbsp;
-                    <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" fill="#FFFFFF" height="20px" width="25px" version="1.1" id="Capa_1" viewBox="0 0 29.978 29.978" space="preserve" stroke="#FFFFFF">
-                        <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
-                        <g id="SVGRepo_iconCarrier"> <g> <path d="M25.462,19.105v6.848H4.515v-6.848H0.489v8.861c0,1.111,0.9,2.012,2.016,2.012h24.967c1.115,0,2.016-0.9,2.016-2.012 v-8.861H25.462z" /> <path d="M14.62,18.426l-5.764-6.965c0,0-0.877-0.828,0.074-0.828s3.248,0,3.248,0s0-0.557,0-1.416c0-2.449,0-6.906,0-8.723 c0,0-0.129-0.494,0.615-0.494c0.75,0,4.035,0,4.572,0c0.536,0,0.524,0.416,0.524,0.416c0,1.762,0,6.373,0,8.742 c0,0.768,0,1.266,0,1.266s1.842,0,2.998,0c1.154,0,0.285,0.867,0.285,0.867s-4.904,6.51-5.588,7.193 C15.092,18.979,14.62,18.426,14.62,18.426z" /> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g>
-                    </svg></Button>
+                <div className='text-center'>
+                    <p className=' text-3xl mb-3'>CONGRATULATIONS!</p>
+                    <h4 className='mb-5'>You've cracked the code on the tablet.</h4>
+                    <h4 className='mt-10'>To get more instructions, download the file</h4>
+                    <Button type="primary" onClick={onDownload} className="mt-5">
+                        Click Here to Download the PDF
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" fill="#FFFFFF" height="20px" width="25px" version="1.1" id="Capa_1" viewBox="0 0 29.978 29.978" space="preserve" stroke="#FFFFFF">
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+                            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+                            <g id="SVGRepo_iconCarrier"> <g> <path d="M25.462,19.105v6.848H4.515v-6.848H0.489v8.861c0,1.111,0.9,2.012,2.016,2.012h24.967c1.115,0,2.016-0.9,2.016-2.012 v-8.861H25.462z" /> <path d="M14.62,18.426l-5.764-6.965c0,0-0.877-0.828,0.074-0.828s3.248,0,3.248,0s0-0.557,0-1.416c0-2.449,0-6.906,0-8.723 c0,0-0.129-0.494,0.615-0.494c0.75,0,4.035,0,4.572,0c0.536,0,0.524,0.416,0.524,0.416c0,1.762,0,6.373,0,8.742 c0,0.768,0,1.266,0,1.266s1.842,0,2.998,0c1.154,0,0.285,0.867,0.285,0.867s-4.904,6.51-5.588,7.193 C15.092,18.979,14.62,18.426,14.62,18.426z" /> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g>
+                        </svg> */}
+                    </Button>
+                </div>
+                <div>
+                </div>
+
             </div>
         </>
     )
--- frontend/src/component/Header/Header.js
+++ frontend/src/component/Header/Header.js
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import Videodown from "../App/Videodown";
 
+
 const Header = () => {
     return (
         <>
             <nav className="bg-blue-950 py-2 md:py-4">
                 <div className="container px-4 mx-auto md:flex md:items-center">
-                    <div className=" text-center">
+                    <div className=" text-center ml-10">
                         {/* <h4 className="text-white">
                             OSPCYBER
                         </h4> */}
-                        <img src="" />
+                        <img src="./logo.png" />
                     </div>
                     <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
                         <Link to="/login" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-transparent rounded hover:bg-sky-800 hover:text-white transition-colors duration-300" >
@@ -25,9 +26,9 @@ const Header = () => {
                         {/* <Link to="/passcode" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-transparent rounded hover:bg-sky-800 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">
                             PassCode
                         </Link> */}
-                        <Link to="/email" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-transparent rounded hover:bg-sky-800 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">
+                        {/* <Link to="/email" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-transparent rounded hover:bg-sky-800 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">
                             Email
-                        </Link>
+                        </Link> */}
                         {/* <Link to="/down" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-transparent rounded hover:bg-sky-800 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">
                             Down
                         </Link> */}
